f53f36cf5e72aa1f6e24316b5531014f2acc2689
jjayoma2017 <jjayoma2017@gmail.com>
2022-06-17 22:38:21 +0800

    style the header section

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ import {
 
 export default function Header() {
   return (
-    <div>
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
       <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
@@ -18,8 +18,9 @@ export default function Header() {
         <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
       </div>
       <Image
+        className="cursor-pointer active:brightness-110"
         width={100}
-        height={100}
+        height={50}
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/640px-IMDB_Logo_2016.svg.png"
       />
     </div>
