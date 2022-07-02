bd9bb51d7991e496b87042b594f393b4fcb4320f
jjayoma2017 <jjayoma2017@gmail.com>
2022-07-02 14:53:01 +0800

    create server side function and pass it to client side

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import Image from 'next/image';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Results from '../components/Results';
+import requests from '../util/requests';
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -19,7 +20,20 @@ export default function Home() {
       <Navbar />
 
       {/* Results */}
-      <Results />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre || 'fetchTrending';
+  const request = await fetch(
+    `https://api.themoviedb.org/3${requests[genre].url}`
+  ).then((response) => response.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
